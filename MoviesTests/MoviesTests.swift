b57bb0354d56288a891e2a54ca5d0fc00b0e959b
kleyson tavares <kleyson.tavares@icloud.com>
2021-03-11 21:32:47 -0300

    implementação dos testes unitarios

--- MoviesTests/MoviesTests.swift
+++ MoviesTests/MoviesTests.swift
@@ -10,25 +10,19 @@ import XCTest
 @testable import Movies
 
 class MoviesTests: XCTestCase {
-
+    
+    var sut: ViewController!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        sut = vc
+        sut.loadView()
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testTitleViewController() {
+    sut.viewDidLoad()
+        XCTAssertEqual(sut.title, "Filmes")
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
